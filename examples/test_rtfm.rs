05a882264cf9f9390bc58337471ff1283174c894
Sergey Matyukevich <geomatsi@gmail.com>
2020-08-07 23:52:12 +0300

    tests: rtic: add scheduled task
    Update RTIC example: add scheduled task.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- examples/test_rtfm.rs
+++ examples/test_rtfm.rs
@@ -4,6 +4,8 @@
 
 use cortex_m as cm;
 use rtic::app;
+use rtic::cyccnt::Instant;
+use rtic::cyccnt::U32Ext;
 use stm32l1xx_hal as hal;
 
 use core::panic::PanicInfo;
@@ -13,7 +15,11 @@ use hal::prelude::*;
 use hal::rcc::Config;
 use hal::stm32;
 
-#[app(device = stm32l1xx_hal::stm32, peripherals = true)]
+/* HSI clock : 16MHz  */
+
+const TEST_PERIOD: u32 = 8_000_000; /* 0.5 sec */
+
+#[app(device = stm32l1xx_hal::stm32, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
     struct Resources {
         led1: hal::gpio::gpiob::PB0<hal::gpio::Output<hal::gpio::PushPull>>,
@@ -21,8 +27,9 @@ const APP: () = {
         tmr2: hal::timer::Timer<stm32::TIM2>,
     }
 
-    #[init]
-    fn init(cx: init::Context) -> init::LateResources {
+    #[init(schedule = [test_task])]
+    fn init(mut cx: init::Context) -> init::LateResources {
+        /* init hardware */
         let mut rcc = cx.device.RCC.freeze(Config::hsi());
 
         let mut tmr2 = cx.device.TIM2.timer(1.hz(), &mut rcc);
@@ -34,6 +41,19 @@ const APP: () = {
 
         led1.set_high().unwrap();
         led2.set_low().unwrap();
+
+        /* enable monotonic timer */
+
+        cx.core.DCB.enable_trace();
+        cx.core.DWT.enable_cycle_counter();
+
+        /* schedule tasks */
+
+        cx.schedule
+            .test_task(Instant::now() + TEST_PERIOD.cycles())
+            .unwrap();
+
+        /* init late resources */
 
         init::LateResources {
             led1: led1,
@@ -49,11 +69,23 @@ const APP: () = {
         }
     }
 
-    #[task(binds = TIM2, resources = [led1, led2, tmr2])]
+    #[task(binds = TIM2, resources = [led1, tmr2])]
     fn tim2(cx: tim2::Context) {
         cx.resources.led1.toggle().unwrap();
-        cx.resources.led2.toggle().unwrap();
         cx.resources.tmr2.clear_irq();
+    }
+
+    #[task(schedule = [test_task], resources = [led2])]
+    fn test_task(cx: test_task::Context) {
+        cx.resources.led2.toggle().unwrap();
+        cx.schedule
+            .test_task(cx.scheduled + TEST_PERIOD.cycles())
+            .unwrap();
+    }
+
+    // needed for RTIC timer queue and task management
+    extern "C" {
+        fn EXTI2();
     }
 };
 
